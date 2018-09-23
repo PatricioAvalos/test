840e1dc56a5cc7fe798ea9e2c25fb885823409c0
Tomino <p.avalos.94@gmail.com>
2018-09-23 00:24:05 -0300

    test 2

--- 0.py
+++ 0.py
@@ -6,3 +6,16 @@ Created on Sat Sep 22 23:53:32 2018
 """
 
 print("Hola")
+
+def f(x):
+    a = x ** 2
+    b = a + g(a)
+    return (a * b)
+
+def g(x):
+    a = x * 3
+    return a ** 2
+
+m = f(1)
+n = g(1)
+print (m, n)
